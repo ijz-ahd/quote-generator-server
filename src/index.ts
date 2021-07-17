3d93a974e5176305140a303b41fcd06712b72b1e
ijz-ahd <ijazahamed1612@gmail.com>
2021-07-18 02:01:07 +0530

    fixed bugs

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import express from "express";
 import authRoute from "./routes/auth";
 import favoriteRoute from "./routes/favorites";
 import cors from "cors";
+import User from "./entity/User";
+import Favorite from "./entity/Favorites";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -22,7 +24,10 @@ app.get("/", (_: express.Request, res: express.Response) =>
 
 app.listen(PORT, async () => {
   try {
-    await createConnection();
+    await createConnection({
+      type: "postgres",
+      entities: [User, Favorite],
+    });
     console.log(`Server running on http://localhost:${PORT}`);
   } catch (err) {
     console.log(err);

ebf92ad8277edbd6e9b264b10bcaafd06461d5ca
ijz-ahd <ijazahamed1612@gmail.com>
2021-07-18 00:39:30 +0530

    bug fix

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import favoriteRoute from "./routes/favorites";
 import cors from "cors";
 import User from "./entity/User";
 import Favorite from "./entity/Favorites";
+import BaseModel from "./entity/BaseModel";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -29,7 +30,7 @@ app.listen(PORT, async () => {
       ssl: {
         rejectUnauthorized: false,
       },
-      entities: [User, Favorite],
+      entities: [User, Favorite, BaseModel],
       url: "postgres://ftiwypnzmkfkld:4d062fda27ceb585b3b1082027d04126f636c2e05490cca6e99c9bba6ae640ee@ec2-52-45-183-77.compute-1.amazonaws.com:5432/ddu2q8mekqva16",
     });
     await connection.runMigrations();

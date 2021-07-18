0e963c8aac71c01e5440869f93edcfcd102f7e62
ijz-ahd <ijazahamed1612@gmail.com>
2021-07-18 12:06:03 +0530

    repository error fix

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ import express from "express";
 import authRoute from "./routes/auth";
 import favoriteRoute from "./routes/favorites";
 import cors from "cors";
-import { User } from "./entity/User";
-import { Favorite } from "./entity/Favorite";
+// import { User } from "./entity/User";
+// import { Favorite } from "./entity/Favorite";
 import { Client } from "pg";
 require("dotenv").config();
 
@@ -28,21 +28,22 @@ app.get("/", (_: express.Request, res: express.Response) =>
 
 app.listen(PORT, async () => {
   try {
-    await createConnection({
-      type: "postgres",
-      port: 5432,
-      entities: [User, Favorite, __dirname + "/../**/**.entity{.ts,.js}"],
-      url: process.env.DB_URL,
-      host: "ec2-52-45-183-77.compute-1.amazonaws.com",
-      username: "ftiwypnzmkfkld",
-      password:
-        "4d062fda27ceb585b3b1082027d04126f636c2e05490cca6e99c9bba6ae640ee",
-      database: "ddu2q8mekqva16",
-      extra: {
-        ssl: true,
-      },
-    });
+    // await createConnection({
+    //   type: "postgres",
+    //   port: 5432,
+    //   entities: [User, Favorite, __dirname + "/../**/**.entity{.ts,.js}"],
+    //   url: process.env.DB_URL,
+    //   host: "ec2-52-45-183-77.compute-1.amazonaws.com",
+    //   username: "ftiwypnzmkfkld",
+    //   password:
+    //     "4d062fda27ceb585b3b1082027d04126f636c2e05490cca6e99c9bba6ae640ee",
+    //   database: "ddu2q8mekqva16",
+    //   extra: {
+    //     ssl: true,
+    //   },
+    // });
 
+    await createConnection();
     const client = new Client({
       connectionString: process.env.DB_URL,
       ssl: {

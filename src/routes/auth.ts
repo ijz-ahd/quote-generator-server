4df3a82c79f51b35360cd3f4dfffbf3fe80e3b49
ijz-ahd <ijazahamed1612@gmail.com>
2021-07-18 13:11:11 +0530

    server config

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -61,7 +61,7 @@ const login = async (req: Request, res: Response) => {
       "Set-Cookie",
       cookie.serialize("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        // secure: process.env.NODE_ENV === "production",
         sameSite: "strict",
         maxAge: 3600 * 24,
         path: "/",

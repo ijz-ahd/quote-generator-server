b0cd7e1e98ee2629da5adba549d8591ee06e4ca6
ijz-ahd <ijazahamed1612@gmail.com>
2021-07-18 13:20:23 +0530

    server config

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "*",
+    origin: "https://quote-generator-3nfu55yb7-ijz-ahd.vercel.app",
     credentials: true,
   })
 );

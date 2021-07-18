72e4688de61b1eb11199ce16542b15ff5cf644c3
ijz-ahd <ijazahamed1612@gmail.com>
2021-07-18 12:53:23 +0530

    client config

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000", process.env.CLIENT_URL],
+    origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
